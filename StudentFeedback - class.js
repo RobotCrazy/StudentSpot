57244a7c4c824586d6da1e75a616a3644bbcb4de
John Anselmo <jjohnanselmo@gmail.com>
2018-07-13 14:27:46 -0400

    Restructure

--- StudentFeedback - class.js
+++
@@ -1,9 +0,0 @@
-class StudentFeedback {
-    constructor(workTime, surviveClass, learn, challenge, expectationDiff) {
-        this.workTime = workTime;
-        this.surviveClass = surviveClass;
-        this.learn = learn;
-        this.challenge = challenge;
-        this.expectationDiff = expectationDiff;
-    }
-}
