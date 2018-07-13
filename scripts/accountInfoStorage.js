65cf22408d6ee692b89c183c23a59f8150ac2fe7
johnanselmo-s3i <jjohnanselmo@gmail.com>
2018-07-13 15:37:59 -0400

    New encryption + decryption method

--- scripts/accountInfoStorage.js
+++ scripts/accountInfoStorage.js
@@ -1,8 +1,19 @@
-var key = "unique";
-// var simpleCrypto = new SimpleCrypto(key);
+import sha256 from 'crypto-js/sha256';
+import hmacSHA512 from 'crypto-js/hmac-sha512';
+import Base64 from 'crypto-js/enc-base64';
+
+const message, nonce, path, privateKey; // ...
+const hashDigest = sha256(nonce + message);
+const hmacDigest = Base64.stringify(hmacSHA512(path + hashDigest, privateKey));
+
+var AES = require("crypto-js/aes");
+var SHA256 = require("crypto-js/sha256");
+
 var plain = "test";
-console.log("Plain text: " + plain);
-var encrypted = CryptoJS.AES.encrypt("Message", key);
-console.log("Encrypted text: " + encrypted);
+var key = "unique";
+var encrypted = CryptoJS.AES.encrypt(plain, key);
 var decrypted = CryptoJS.AES.decrypt(encrypted, key);
-console.log("Decrypted text: " + decrypted);
+
+console.log("Plain text: " + plain);
+console.log("Encrypted: " + encrypted);
+console.log("Decrypted: " + decrypted);
