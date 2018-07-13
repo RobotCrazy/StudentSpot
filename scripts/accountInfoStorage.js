19138ac61a790783fa23a3d7cdba48ae1dd9b3ce
johnanselmo-s3i <jjohnanselmo@gmail.com>
2018-07-13 15:08:39 -0400

    Test accountInfoStorage script

---
+++ scripts/accountInfoStorage.js
@@ -0,0 +1,9 @@
+import SimpleCrypto from "simple-crypto-js";
+
+var key = "unique";
+var simpleCrypto = new SimpleCrypto(key);
+var plain = "test";
+console.log("Plain text: " + plain);
+var encrypted = simpleCrypto.encrypt(plain);
+console.log("Encrypted text: " + encrypted);
+var decrypted = simpleCrypto.decrypt(encrypted);
