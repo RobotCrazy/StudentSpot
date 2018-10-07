8bdaea5a936cb6d76854eddf82e27da1ce50535d
Upneet <captainus55@gmail.com>
2018-10-07 17:32:02 -0400

    Updates for testing

--- scripts/questionFormScript.js
+++ scripts/questionFormScript.js
@@ -35,3 +35,48 @@ function makeApiCall() {
 function buildQuestionForm() {
     makeApiCall();
 }
+
+function initClient() {
+    console.log("initClient happened");
+    var API_KEY = 'AIzaSyAkmxR79hMxP2FWNe5e3oMHBYI8Jk8gbWE'; // TODO: Update placeholder with desired API key.
+
+    var CLIENT_ID = '919400316199-68vns4ia05nloajcjtcl0qqqq54qbul2.apps.googleusercontent.com'; // TODO: Update placeholder with desired client ID.
+
+    // TODO: Authorize using one of the following scopes:
+    //   'https://www.googleapis.com/auth/drive'
+    //   'https://www.googleapis.com/auth/drive.file'
+    //   'https://www.googleapis.com/auth/drive.readonly'
+    //   'https://www.googleapis.com/auth/spreadsheets'
+    //   'https://www.googleapis.com/auth/spreadsheets.readonly'
+    var SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
+
+
+    gapi.client.init({
+        'apiKey': API_KEY,
+        'clientId': CLIENT_ID,
+        'scope': SCOPE,
+        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(function() {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    }).catch(function(fromResolve) {
+        console.log(fromResolve);
+    });
+}
+
+function handleClientLoad() {
+    console.log("handleClientLoad");
+    gapi.load('client:auth2', initClient);
+}
+
+/**
+ * 
+ * This function makes calls to the API for each subject and collects data from each subject.  
+ */
+function updateSignInStatus(isSignedIn) {
+    makeApiCall("English");
+    makeApiCall("Social Studies");
+    makeApiCall("Math");
+    makeApiCall("Science");
+    makeApiCall("IT/CTE");
+}
