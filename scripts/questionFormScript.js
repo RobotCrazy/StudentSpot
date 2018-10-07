76115829f5952ae474d629aad3a8e187a07d332f
Upneet <captainus55@gmail.com>
2018-10-07 17:27:19 -0400

    Student Feedback Form edits

--- scripts/questionFormScript.js
+++ scripts/questionFormScript.js
@@ -1,6 +1,4 @@
 function makeApiCall() {
-    var studentFeedbackApiCallComplete = false;
-    console.log("studentFeedbackAPICall() has been called");
     var paramsStudent = {
         // The ID of the spreadsheet to retrieve data from.
         spreadsheetId: '1vvAMq9n8vQAa6Q8S51x3J5Bqmg1OVFX6LFNjECdNZ-E', // TODO: Update placeholder value.
@@ -21,33 +19,16 @@ function makeApiCall() {
 
     requestStudent = gapi.client.sheets.spreadsheets.values.get(paramsStudent);
     requestStudent.then(function(response) {
-        // TODO: Change code below to process the `response` object:
-        //console.log("Student Data has been succesfully pulled");
-        for (var i = 0; i < courses.length; i++) {
-            var courseName = courses[i].name;
+            // TODO: Change code below to process the `response` object:
+            console.log(response);
             for (var x = 0; x < response.result.values.length; x++) {
-                if (courseName == response.result.values[x][1]) {
-                    /*console.log("Course name: " + courseName + "; " + response.result.values[x][1]);
-                    console.log("success");*/
-                    var newStudentFeedback = buildStudentFeedback(response.result.values[x]);
-                    courses[i].addStudentFeedback(newStudentFeedback);
-                    //console.log("student is happening");
-                    /*console.log(courses[i].studentFeedback);*/
-                }
+
             }
-
-        }
-        /*console.log("studentAPICall is being set to true");
-        studentFeedbackApiCallComplete = true;*/
-        //displayCourses(courses);
-    }, function(reason) {
-        console.error('error: ' + reason.result.error.message);
-    });
-    /*while (studentFeedbackApiCallComplete == false) {
-
-    }*/
-    console.log("studentFeedbackAPICall() is done");
-    studentFeedbackComplete = true;
+        },
+        function(reason) {
+            console.error('error: ' + reason.result.error.message);
+            window.refresh();
+        });
 
 }
 
