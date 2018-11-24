85cf407c2e2e4ce1e8c3a5290592022f86c45514
s3id0n <jjohnanselmo@gmail.com>
2018-11-24 17:30:04 -0500

    Todo

--- scripts/accessAccounts.php
+++ scripts/accessAccounts.php
@@ -3,5 +3,12 @@
 $accs[] = "john";
 $accs[] = "upneet";
 
+$passes[] = "password";
+$passes[] = "password";
 
+
+//loop through both comparing to the inputted values in the javascript function on createAccount.html
+//if they're equal, then return a true value
+//javascript function will access the value, if it is true, then it will proceed to the next page
+//if false, alert - incorrect username or password
 ?>
