bbbccf4cd4142ec1d6c0806df6ad2d613ff1058a
johnanselmo-s3i <jjohnanselmo@gmail.com>
2018-11-25 12:23:39 -0500

    Fix paths

--- scripts/bundleLogin.js
+++ scripts/bundleLogin.js
@@ -7,7 +7,7 @@
     loginButton.onclick = function() {
       const fs = require('fs');
       try {
-        var data = fs.readFileSync('./accounts/adminLogins.txt', 'utf8');
+        var data = fs.readFileSync('../accounts/adminLogins.txt', 'utf8');
         console.log(data);
       } catch (e) {
         console.log('Error:', e.stack);
