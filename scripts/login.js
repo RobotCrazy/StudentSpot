5a61d5a813e6d4abfd5431f35f22ddbc921d0a10
johnanselmo-s3i <jjohnanselmo@gmail.com>
2018-11-25 13:07:48 -0500

    Change syntax for browserify-fs

--- scripts/login.js
+++ scripts/login.js
@@ -19025,8 +19025,9 @@ function login() {
   console.log("Works here");
   const fs = require('browserify-fs');
   try {
-    var data = fs.readFileSync('./accounts/adminLogins.txt', 'utf8');
-    console.log(data);
+    fs.readFile('./accounts/adminLogins.txt', 'utf8', function(err, data) {
+      console.log(data);
+    });
   } catch (e) {
     console.log('Error:', e.stack);
   }
