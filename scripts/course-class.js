9ba595a1df7e4597c9cdfde418af100c118cd9e9
Upneet <captainus55@gmail.com>
2019-01-07 21:53:04 -0500

    Fixing live page

--- scripts/course-class.js
+++ scripts/course-class.js
@@ -19,7 +19,7 @@ class Course {
     getQuestions() {
         var questions = new Array(0);
         console.log("getting questions");
-        for (let i = 0; i < this.questionAnswerList.length; i += 2) {
+        for (let i = 0; i < this.questionAnswerList.length; i++) {
             console.log(this.questionAnswerList[i]);
             questions.push(this.questionAnswerList[i]);
         }
