e91805c44a26602fe5264233bd35bd7588878846
Upneet <captainus55@gmail.com>
2019-01-14 19:50:15 -0500

    Modifications

--- scripts/googledatahandling.js
+++ scripts/googledatahandling.js
@@ -109,7 +109,7 @@ function writeToSheet(accessToken) {
         // How the input data should be interpreted.
         valueInputOption: 'RAW', // TODO: Update placeholder value.
 
-        accessToken: accessToken
+        access_token: accessToken
     };
 
     var valueRangeBody = {
