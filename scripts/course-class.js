18800a941149a6fbb76cec293821a804891cdd41
Upneet <captainus55@gmail.com>
2019-02-11 23:05:42 -0500

    Modifications

--- scripts/course-class.js
+++ scripts/course-class.js
@@ -2,12 +2,11 @@
  * The Course class is used to store all necessary info about the courses
  */
 class Course {
-    constructor(name, subject, credits, countyDesc, questionAnswerList) {
+    constructor(name, subject, questions, answers) {
         this.name = name;
         this.subject = subject;
-        this.credits = credits;
-        this.countyDesc = countyDesc;
-        this.questionAnswerList = questionAnswerList;
+        this.questions = questions;
+        this.answers = answers;
         this.studentFeedback = new Array(0);
     }
     addStudentFeedback(studentFeedback) {
@@ -17,15 +16,7 @@ class Course {
         this.questionInputTypes = questionInputTypes;
     }
     getQuestions() {
-        var questions = new Array(0);
-        console.log("getting questions");
-        console.log(this.questionAnswerList.length);
-        for (let i = 0; i < this.questionAnswerList.length; i += 2) {
-            console.log(this.questionAnswerList[i]);
-            questions.push(this.questionAnswerList[i]);
-        }
-        console.log("done getting questions");
-        return questions;
+        return this.questions;
     }
 
 }
